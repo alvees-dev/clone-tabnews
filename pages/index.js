5b705dac0f33f933d361959da3e21ca84511d2e3
Gabriel Alves <163564842+alvees-dev@users.noreply.github.com>
2025-12-13 01:59:29 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Livia, eu amo você. Se você me ama, me manda uma foto sorrindo</h1>
+    return <h1>Livia, eu amo você. Se você me ama, me manda uma foto sorrindo.</h1>
 }
 
 export default Home;
