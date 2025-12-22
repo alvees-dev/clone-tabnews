12110b7fb4d449b9b3d5227a0dadce42a24bce42
Gabriel Alves <163564842+alvees-dev@users.noreply.github.com>
2025-12-22 23:23:28 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Livia, eu amo você. Se você me ama, me manda uma foto sorrindo.</h1>
+  return (
+    <h1>Livia, eu amo você. Se você me ama, me manda uma foto sorrindo.</h1>
+  );
 }
 
 export default Home;
