d31795ea54c367708106d27b4c6fd5fc96ceb769
Gabriel Alves <163564842+alvees-dev@users.noreply.github.com>
2026-07-09 04:17:24 +0000

    fix text

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <h1>
-      Estou criando este site com objetivo de adiquirir conhecimento e me tornar
+      Estou criando este site com objetivo de adquirir conhecimento e me tornar
       um profissional mais capacitado a criar projetos de ponta à ponta!
     </h1>
   );
