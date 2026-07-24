929621b778da3414f02a0a30b80b00a8d4c30ee3
Gabriel Alves <163564842+alvees-dev@users.noreply.github.com>
2026-07-24 18:54:22 +0000

    adds `/api/v1/migrations`endpoint

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+dotenv = require("dotenv");
+
+const nextJest = require("next/jest");
+
+dotenv.config({
+  path: "./.env.development",
+});
+
+const createJestConfig = nextJest();
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
